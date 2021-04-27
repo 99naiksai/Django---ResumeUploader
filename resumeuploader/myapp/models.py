f9196ac883469030b09494546a1faba5c4287ff5
Sai <99naiksai@gmail.com>
2021-04-28 00:25:58 +0530

    Initial Commit

---
+++ resumeuploader/myapp/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+STATE_CHOICE = (
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Karnataka','Karnataka'),
+    ('Kerala','Kerala'),
+    ('Chhattisgarh','Chhattisgarh'),
+    ('Uttar Pradesh','Uttar Pradesh'),
+    ('Goa','Goa'),
+    ('Gujarat','Gujarat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Jammu and Kashmir','Jammu and Kashmir'),
+    ('Jharkhand','Jharkhand'),
+    ('West Bengal','West Bengal'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalaya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Orissa','Orissa'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajashtan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttarakhand','Uttarakhand'),
+)
+# Create your models here.
+class Resume(models.Model):
+    name = models.CharField(max_length=200)
+    dob = models.DateField(auto_now=False , auto_now_add=False)
+    gender = models.CharField(max_length=100)
+    locality = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    pin = models.PositiveIntegerField()
+    state = models.CharField(choices = STATE_CHOICE, max_length=50)
+    mobile = models.PositiveIntegerField()
+    email = models.EmailField()
+    job_city = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to='profileimg' , blank=True)
+    my_file = models.FileField(upload_to='doc' , blank=True)
+    
